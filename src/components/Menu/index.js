642c30220c1023475c360e32eee22a4abe37c2cd
Felipe Martins <felipemjesus@outlook.com>
2020-07-28 20:53:58 -0300

    Criando componente ButtonLink

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Logo from '../../assets/img/logo.png'
 import './Menu.css'
+import ButtonLink from './components/ButtonLink'
 
 export default () => {
   return (
@@ -8,6 +9,9 @@ export default () => {
       <a href="/">
         <img className="Logo" src={Logo} alt="Logo FakeFlix" />
       </a>
+      <ButtonLink className="ButtonLink" href="/">
+        Novo vídeo
+      </ButtonLink>
     </nav>
   );
 }
