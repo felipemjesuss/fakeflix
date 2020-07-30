093026aaeaaabb058cd39e5d49c25fb2e7805fd6
Felipe Martins <felipemjesus@outlook.com>
2020-07-30 00:07:36 -0300

    Criado componente de categoria

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './index.css';
 import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria'
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={() => 'Página 404'} />
     </Switch>
   </BrowserRouter>,
---
+++ src/pages/cadastro/Categoria/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PageDefault from '../../../components/PageDefault'
+
+export default () => {
+    return (
+        <PageDefault>
+            <h1>Cadastro de Categoria</h1>
+        </PageDefault>
+    )
+}
