c86054db141aca0e4235748511606e2eba15d33f
Felipe Martins <felipemjesus@outlook.com>
2020-07-30 22:11:22 -0300

    Recuperando dados do form e listando

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,23 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageDefault from '../../../components/PageDefault'
 
 export default () => {
+    const valoresIniciais = {nome: '', cor: '', descricao: ''}
+    const [categorias, setCategorias] = useState([])
+    const [categoria, setCategoria] = useState(valoresIniciais)
+
+    function setValor(campo, valor) {
+        setCategoria({
+            ...categoria,
+            [campo]: valor
+        })
+    }
+
+    function handleChange(e) {
+        const { getAttribute, value } = e.target
+        setValor(getAttribute('name'), value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setCategorias([
+            ...categorias, 
+            categoria
+        ])
+        setCategoria(valoresIniciais)
+    }
+
     return (
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+            <h1>Cadastro de Categoria: {categoria.nome}</h1>
 
-            <form>
-                <label>
-                    Nome da Categoria:
-                    <input
-                        type="text"
-                    />
-                </label>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>
+                        Nome:
+                        <input
+                            type="text"
+                            name="nome"
+                            value={categoria.nome}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Cor:
+                        <input
+                            type="color"
+                            name="cor"
+                            value={categoria.cor}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Descrição:
+                        <textarea
+                            type="text"
+                            name="descricao"
+                            value={categoria.descricao}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div>
 
                 <button>
                     Cadastrar
                 </button>
             </form>
+
+            <ul>
+                {categorias.map((categoria, i) => {
+                    return (
+                        <li key={`${categoria}${i}`}>
+                            {categoria.nome}
+                        </li>
+                    )
+                })}
+            </ul>
         </PageDefault>
     )
 }
